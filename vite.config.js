1e04bb7439924933d1865d170f630331482efb12
leekeifung <leekeifung@gmail.com>
2026-09-09 15:02:05 +0800

    math app try

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/math-functions-app/', // Add this line!
   plugins: [
     react(),
     tailwindcss()
